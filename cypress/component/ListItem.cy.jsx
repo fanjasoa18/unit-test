0439d0467b6c0fbb80716491e83251359ece7243
fanjasoa <fanjasoa18@gmail.com>
2023-03-01 05:20:28 +0300

    chore: add component test

--- cypress/component/ListItem.cy.jsx
+++ cypress/component/ListItem.cy.jsx
@@ -6,5 +6,25 @@ describe('<ListItem />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     mount(<ListItem />)
+  });
+
+  it('add todo', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#add-todo').type(`Lorem ipsum`);
+    cy.get('Enter').click()
+    cy.get('list-item').contains(`Lorem ipsum`)
+  });
+
+  it('move todo to done', () => {
+    cy.get('list-item').type(`Lorem ipsum`)
+    cy.get('list-item li:first-child input[type="checkbox"]').check()
+    cy.get('list-item').contains(`Lorem ipsum`)
   })
 })
+
+
+
+ 
+
+ 
+
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,10 +2,6 @@ export function Input(props) {
     const { onChange, onKeyUp } = props;
 
     return (
-<<<<<<< HEAD
         <input id='add-todo' type="text" onChange={onChange} onKeyUp={onKeyUp}/>
-=======
-        <input type="text" id="add-todo" onChange={onChange} onKeyUp={onKeyUp}/>
->>>>>>> b8e0f9d (feat: callback test for checkbox)
     );
 }
